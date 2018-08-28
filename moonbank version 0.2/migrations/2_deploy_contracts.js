9960782c3affdd2bc77c7e774098576db0a57527
thometh <twalker@lapbot.local>
2018-08-27 23:10:33 -0400

    Removed folder

--- moonbank version 0.2/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-// var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-var Bank = artifacts.require("./Bank.sol");
-
-module.exports = function(deployer) {
-  // deployer.deploy(SimpleStorage);
-  deployer.deploy(Bank);
-};
