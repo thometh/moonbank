790111a00e9aa5ab46ef2d39186379e6c943d4bc
thometh <twalker@lapbot.local>
2018-08-27 23:30:49 -0400

    Added OpenZeppelin

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -1,14 +1,16 @@
 pragma solidity ^0.4.21;
 
-contract Bank {
+import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+
+contract Bank is StandardToken {
+string public name = "moonbank";
+string public symbol = "moonbucks";
+uint public decimals = 0;
+
     // Track how many tokens are owned by each address.
     mapping (address => uint256) public balances;
     mapping (address => bool) astronauts;
     mapping(address => mapping(address => uint256)) public allowed;
-
-    string public name = "moonbank";
-    string public symbol = "moonbucks";
-    uint8 public decimals = 0;
 
     uint256 public totalSupply = 1000;
     uint256 public cashSupply = 1000;
@@ -16,63 +18,10 @@ contract Bank {
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
-    function allowance(address _owner,address _spender) public view returns (uint256)
-    {
-        return allowed[_owner][_spender];
-    }
-
     constructor() public {
         // Initially assign all tokens to the contract's creator.
         balances[msg.sender] = totalSupply;
         emit Transfer(address(0), msg.sender, totalSupply);
-    }
-
-    function transfer(address to, uint256 value) public returns (bool success) {
-        require(balances[msg.sender] >= value);
-
-        balances[msg.sender] -= value;  // deduct from sender's balance
-        balances[to] += value;          // add to recipient's balance
-        emit Transfer(msg.sender, to, value);
-        return true;
-    }
-
-    function approve(address spender, uint256 value) public returns (bool success)
-    {
-        allowed[msg.sender][spender] = value;
-        emit Approval(msg.sender, spender, value);
-        return true;
-    }
-
-    function transferFrom(address from, address to, uint256 value) public returns (bool success)
-    {
-        require(value <= balances[from]);
-        require(value <= allowed[from][msg.sender]);
-        require(to != address(0));  //not burn
-
-        balances[from] -= value;
-        balances[to] += value;
-        allowed[from][msg.sender] -= value;
-        emit Transfer(from, to, value);
-        return true;
-    }
-
-
-    function burn(uint256 amount) public {
-        require(amount <= balances[msg.sender]);
-
-        totalSupply -= amount;
-        balances[msg.sender] -= amount;
-        emit Transfer(msg.sender, address(0), amount);
-    }
-
-    function burnFrom(address from, uint256 amount) public {
-        require(amount <= balances[from]);
-        require(amount <= allowed[from][msg.sender]);
-
-        totalSupply -= amount;
-        balances[from] -= amount;
-        allowed[from][msg.sender] -= amount;
-        emit Transfer(from, address(0), amount);
     }
 
     function balanceOf(address _owner) public view returns (uint256) {
@@ -94,7 +43,6 @@ contract Bank {
         return true;
     }
 
-
     function rideLunarModule(address to, uint256 value) public returns (bool success) {
         require(balances[msg.sender] >= value);
         require(value == 9);
@@ -104,6 +52,5 @@ contract Bank {
         emit Transfer(msg.sender, to, value);
         return true;
     }
-
 
 }
